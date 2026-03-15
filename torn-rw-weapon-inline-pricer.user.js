0e0a70ccb00289ba46943cad6edac6305623c8da
russianrob <robert.n.hay@gmail.com>
2026-03-15 23:15:52 +0000

    v2.6.1 – Test Greasy Fork sync

--- torn-rw-weapon-inline-pricer.user.js
+++ torn-rw-weapon-inline-pricer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn RW Pricer
 // @namespace    torn.rw.weapon.inline.pricer
-// @version      2.6
+// @version      2.6.1
 // @description  Inline price badges for RW weapons and armour using daily-refreshed auction data
 // @author       RussianRob
 // @match        https://www.torn.com/item*
