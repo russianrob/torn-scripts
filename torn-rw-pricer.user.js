f9b276e43942d3fd4506cc4271aa788f49556b09
russianrob <robert.n.hay@gmail.com>
2026-03-16 01:08:49 +0000

    v2.9.8 – Full BONUS_COLOR_RANGES cross-check & correction using forum reference data

--- torn-rw-pricer.user.js
+++ torn-rw-pricer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn RW Pricer
 // @namespace    torn.rw.weapon.inline.pricer
-// @version      2.9.7
+// @version      2.9.8
 // @description  Inline price badges for RW weapons and armour using daily-refreshed auction data
 // @author       RussianRob
 // @match        https://www.torn.com/item*
@@ -73,7 +73,7 @@
 
     var WEAPON_CLASS = {"AK74U":"Pistol / SMG","Enfield SA-80":"Shotgun / Rifle","SIG 552":"Shotgun / Rifle","USP":"Pistol / SMG","SKS Carbine":"Shotgun / Rifle","Cobra Derringer":"Pistol / SMG","Vektor CR-21":"Shotgun / Rifle","Macana":"Melee","Kodachi":"Melee","9mm Uzi":"Pistol / SMG","Heckler & Koch SL8":"Shotgun / Rifle","Type 98 Anti Tank":"Heavy","M249 SAW":"Heavy","Raven MP25":"Pistol / SMG","Desert Eagle":"Pistol / SMG","Ruger 57":"Pistol / SMG","ArmaLite M-15A4":"Shotgun / Rifle","Beretta M9":"Pistol / SMG","XM8 Rifle":"Shotgun / Rifle","Benelli M4 Super":"Shotgun / Rifle","Sai":"Melee","Swiss Army Knife":"Melee","Claymore Sword":"Melee","Yasukuni Sword":"Melee","Diamond Bladed Knife":"Melee","Metal Nunchaku":"Melee","Qsz-92":"Pistol / SMG","Kitchen Knife":"Melee","Mag 7":"Shotgun / Rifle","BT MP9":"Pistol / SMG","China Lake":"Heavy","Negev NG-5":"Heavy","Glock 17":"Pistol / SMG","Cricket Bat":"Melee","Blunderbuss":"Shotgun / Rifle","Ithaca 37":"Shotgun / Rifle","Tavor TAR-21":"Shotgun / Rifle","Samurai Sword":"Melee","RPG Launcher":"Heavy","Naval Cutlass":"Melee","Butterfly Knife":"Melee","TMP":"Pistol / SMG","Hammer":"Melee","Bo Staff":"Melee","M16 A2 Rifle":"Shotgun / Rifle","Jackhammer":"Shotgun / Rifle","Springfield 1911":"Pistol / SMG","Crowbar":"Melee","Katana":"Melee","Scimitar":"Melee","Ninja Claws":"Melee","Kama":"Melee","Leather Bullwhip":"Melee","Dagger":"Melee","Knuckle Dusters":"Melee","Beretta 92FS":"Pistol / SMG","Taurus":"Pistol / SMG","MP 40":"Pistol / SMG","MP5k":"Pistol / SMG","Baseball Bat":"Melee","M4A1 Colt Carbine":"Shotgun / Rifle","AK-47":"Shotgun / Rifle","Thompson":"Pistol / SMG","Magnum":"Pistol / SMG","Skorpion":"Pistol / SMG","Steyr AUG":"Shotgun / Rifle","S&W Revolver":"Pistol / SMG","Sawed-Off Shotgun":"Shotgun / Rifle","Spear":"Melee","Benelli M1 Tactical":"Shotgun / Rifle","Lorcin 380":"Pistol / SMG","Wooden Nunchaku":"Melee","Chain Whip":"Melee","Fiveseven":"Pistol / SMG","Axe":"Melee","Stoner 96":"Heavy","Flail":"Melee","SMAW Launcher":"Heavy","Minigun":"Heavy","PKM":"Heavy","Pen Knife":"Melee","Luger":"Pistol / SMG","Guandao":"Melee","Frying Pan":"Melee","Milkor MGL":"Heavy","Sledgehammer":"Melee","Rheinmetall MG 3":"Heavy","Bread Knife":"Melee","Poison Umbrella":"Melee","Nock Gun":"Shotgun / Rifle","Snow Cannon":"Heavy","Wand of Destruction":"Melee","Bushmaster Carbon 15":"Pistol / SMG","Twin Tiger Hooks":"Melee","Wushu Double Axes":"Melee","Dual Scimitars":"Melee","Dual Samurai Swords":"Melee","Pair of High Heels":"Melee","Gold Plated AK-47":"Shotgun / Rifle","Handbag":"Melee","Slingshot":"Pistol / SMG","Pillow":"Melee","Dual TMPs":"Pistol / SMG","Dual Bushmasters":"Pistol / SMG","Dual MP5s":"Pistol / SMG","Dual P90s":"Pistol / SMG","Dual Uzis":"Pistol / SMG","Golden Broomstick":"Melee","Devil's Pitchfork":"Melee","Pair of Ice Skates":"Melee","Diamond Icicle":"Melee","Petrified Humerus":"Melee","Plastic Sword":"Melee","Madball":"Melee"};
 
-    var BONUS_ID_MAP = {"1":"Expose","14":"Proficience","20":"Stricken","21":"Plunder","33":"Blindfire","34":"Hazardous","35":"Spray","36":"Demoralize","37":"Storage","38":"Freeze","41":"Revitalize","42":"Wither","43":"Roshambo","44":"Slow","45":"Cripple","46":"Weaken","47":"Cupid","48":"Throttle","49":"Crusher","50":"Achilles","51":"Blindside","52":"Backstab","53":"Grace","54":"Berserk","55":"Conserve","56":"Eviscerate","57":"Bleed","58":"Stun","59":"Paralyze","60":"Suppress","61":"Motivation","62":"Deadly","63":"Deadeye","64":"Fury","65":"Rage","66":"Puncture","67":"Comeback","68":"Powerful","71":"Specialist","72":"Assassinate","73":"Smurf","74":"Double-Edged","75":"Execute","76":"Wind-Up","78":"Sure Shot","79":"Focus","80":"Frenzy","81":"Warlord","82":"Finale","83":"HomeRun","84":"Parry","85":"Bloodlust","86":"Disarm","87":"Empower","88":"Quicken","89":"Lacerate","101":"Penetrate","102":"Irradiate","103":"Toxin","104":"Smash","105":"Double-Tap","120":"Shock"};
+    var BONUS_ID_MAP = {"1":"Expose","14":"Proficience","20":"Stricken","21":"Plunder","33":"Blindfire","34":"Hazardous","35":"Spray","36":"Demoralize","37":"Storage","38":"Freeze","41":"Revitalize","42":"Wither","43":"Roshambo","44":"Slow","45":"Cripple","46":"Weaken","47":"Cupid","48":"Throttle","49":"Crusher","50":"Achilles","51":"Blindside","52":"Backstab","53":"Grace","54":"Berserk","55":"Conserve","56":"Eviscerate","57":"Bleed","58":"Stun","59":"Paralyze","60":"Suppress","61":"Motivation","62":"Deadly","63":"Deadeye","64":"Fury","65":"Rage","66":"Puncture","67":"Comeback","68":"Powerful","71":"Specialist","72":"Assassinate","73":"Smurf","74":"Double-Edged","75":"Execute","76":"Wind-Up","78":"Sure Shot","79":"Focus","80":"Frenzy","81":"Warlord","82":"Finale","83":"HomeRun","84":"Parry","85":"Bloodlust","86":"Disarm","87":"Empower","88":"Quicken","89":"Lacerate","90":"Radiation Protection","91":"Invulnerable","92":"Insurmountable","101":"Penetrate","102":"Irradiate","103":"Toxin","104":"Smash","105":"Double-Tap","112":"Kinetokinesis","115":"Immutable","120":"Shock","121":"Irrepressible"};
 
     var ITEM_ID_MAP = {"1":"Hammer","2":"Baseball Bat","3":"Crowbar","4":"Knuckle Dusters","5":"Pen Knife","6":"Kitchen Knife","7":"Dagger","8":"Axe","9":"Scimitar","11":"Samurai Sword","12":"Glock 17","13":"Raven MP25","14":"Ruger 57","15":"Beretta M9","16":"USP","17":"Beretta 92FS","18":"Fiveseven","19":"Magnum","20":"Desert Eagle","22":"Sawed-Off Shotgun","23":"Benelli M1 Tactical","24":"MP5 Navy","25":"P90","26":"AK-47","27":"M4A1 Colt Carbine","28":"Benelli M4 Super","29":"M16 A2 Rifle","30":"Steyr AUG","31":"M249 SAW","63":"Minigun","99":"Springfield 1911","100":"Egg Propelled Launcher","108":"9mm Uzi","109":"RPG Launcher","110":"Leather Bullwhip","111":"Ninja Claws","146":"Yasukuni Sword","173":"Butterfly Knife","174":"XM8 Rifle","177":"Cobra Derringer","189":"S&W Revolver","217":"Claymore Sword","219":"Enfield SA-80","223":"Jackhammer","224":"Swiss Army Knife","225":"Mag 7","227":"Spear","228":"Vektor CR-21","231":"Heckler & Koch SL8","233":"BT MP9","234":"Chain Whip","235":"Wooden Nunchaku","236":"Kama","237":"Kodachi","238":"Sai","240":"Type 98 Anti Tank","243":"Taurus","245":"Bo Staff","247":"Katana","248":"Qsz-92","249":"SKS Carbine","252":"Ithaca 37","253":"Lorcin 380","254":"S&W M29","289":"Dual Axes","290":"Dual Hammers","391":"Macana","395":"Metal Nunchaku","397":"Flail","398":"SIG 552","399":"ArmaLite M-15A4","400":"Guandao","402":"Ice Pick","438":"Cricket Bat","439":"Frying Pan","483":"MP5k","484":"AK74U","485":"Skorpion","486":"TMP","487":"Thompson","488":"MP 40","489":"Luger","490":"Blunderbuss","612":"Tavor TAR-21","613":"Harpoon","614":"Diamond Bladed Knife","615":"Naval Cutlass","830":"Nock Gun","831":"Beretta Pico","832":"Riding Crop","837":"Rheinmetall MG 3","838":"Homemade Pocket Shotgun","846":"Scalpel","850":"Sledgehammer","1053":"Bread Knife","1055":"Poison Umbrella","1152":"SMAW Launcher","1153":"China Lake","1154":"Milkor MGL","1155":"PKM","1156":"Negev NG-5","1157":"Stoner 96","1158":"Meat Hook","1159":"Cleaver","1231":"Golf Club","1255":"Bone Saw","1257":"Cattle Prod","1456":"Bolas","76":"Snow Cannon","170":"Wand of Destruction","241":"Bushmaster Carbon 15","250":"Twin Tiger Hooks","251":"Wushu Double Axes","291":"Dual Scimitars","292":"Dual Samurai Swords","346":"Pair of High Heels","382":"Gold Plated AK-47","387":"Handbag","393":"Slingshot","440":"Pillow","545":"Dual TMPs","546":"Dual Bushmasters","547":"Dual MP5s","548":"Dual P90s","549":"Dual Uzis","599":"Golden Broomstick","600":"Devil's Pitchfork","604":"Pair of Ice Skates","605":"Diamond Icicle","632":"Petrified Humerus","790":"Plastic Sword","839":"Madball"};
 
@@ -102,58 +102,58 @@
 
     // ─── Bonus Color Ranges (per-bonus level → color tier) ───
     var BONUS_COLOR_RANGES = {
-        '50':  { yellow: [50,73],   orange: [77,98],   red: [114,149] },
-        '72':  { yellow: [50,69],   orange: [70,93],   red: [110,148] },
-        '52':  { yellow: [30,40],   orange: [45,52],   red: [79,96] },
-        '54':  { yellow: [20,34],   orange: [39,53],   red: [60,87] },
-        '57':  { yellow: [20,30],   orange: [31,44],   red: [53,67] },
-        '51':  { yellow: [25,37],   orange: [41,59],   red: [73,96] },
-        '85':  { yellow: [10,11],   orange: [12,14],   red: [17,17] },
+        '50':  { yellow: [50,73],   orange: [77,98],   red: [114,142] },
+        '72':  { yellow: [50,69],   orange: [70,93],   red: [107,141] },
+        '52':  { yellow: [30,40],   orange: [45,52],   red: [79,79] },
+        '54':  { yellow: [20,30],   orange: [39,53],   red: [60,60] },
+        '57':  { yellow: [20,30],   orange: [31,45],   red: [53,62] },
+        '51':  { yellow: [25,38],   orange: [40,59],   red: [70,82] },
+        '85':  { yellow: [10,12],   orange: [12,14],   red: [17,17] },
         '67':  { yellow: [50,66],   orange: [70,99],   red: [102,127] },
-        '55':  { yellow: [25,29],   orange: [30,36],   red: [43,49] },
-        '45':  { yellow: [20,28],   orange: [29,40],   red: [52,58] },
+        '55':  { yellow: [25,29],   orange: [30,36],   red: [41,49] },
+        '45':  { yellow: [20,27],   orange: [29,40],   red: [52,58] },
         '49':  { yellow: [50,72],   orange: [76,102],  red: [133,133] },
         '47':  { yellow: [50,74],   orange: [75,110],  red: [124,157] },
-        '63':  { yellow: [25,45],   orange: [46,72],   red: [76,123] },
-        '62':  { yellow: [2,3],     orange: [4,6],     red: [9,10] },
-        '86':  { yellow: [3,4],     orange: [5,9],     red: [10,15] },
-        '74':  { yellow: [10,14],   orange: [16,22],   red: [23,32] },
-        '105': { yellow: [15,23],   orange: [25,35],   red: [40,54] },
-        '87':  { yellow: [52,85],   orange: [90,140],  red: [180,206] },
-        '56':  { yellow: [15,18],   orange: [19,24],   red: [26,34] },
-        '75':  { yellow: [15,17],   orange: [18,22],   red: [23,28] },
-        '1':   { yellow: [7,9],     orange: [10,13],   red: [14,21] },
-        '82':  { yellow: [10,11],   orange: [12,12],   red: [13,17] },
-        '79':  { yellow: [15,19],   orange: [20,24],   red: [26,32] },
-        '80':  { yellow: [5,6],     orange: [7,9],     red: [10,14] },
+        '63':  { yellow: [25,45],   orange: [45,72],   red: [76,118] },
+        '62':  { yellow: [2,3],     orange: [3,5],     red: [8,9] },
+        '86':  { yellow: [3,5],     orange: [5,9],     red: [9,15] },
+        '74':  { yellow: [10,14],   orange: [16,22],   red: [32,32] },
+        '105': { yellow: [15,23],   orange: [25,35],   red: [40,40] },
+        '87':  { yellow: [50,85],   orange: [90,140],  red: [156,206] },
+        '56':  { yellow: [15,19],   orange: [19,25],   red: [26,32] },
+        '75':  { yellow: [15,18],   orange: [18,22],   red: [23,29] },
+        '1':   { yellow: [7,9],     orange: [9,14],    red: [14,19] },
+        '82':  { yellow: [10,11],   orange: [12,13],   red: [17,17] },
+        '79':  { yellow: [15,19],   orange: [19,24],   red: [26,32] },
+        '80':  { yellow: [5,7],     orange: [7,10],    red: [11,12] },
         '64':  { yellow: [10,15],   orange: [16,23],   red: [26,34] },
-        '53':  { yellow: [20,31],   orange: [38,49],   red: [60,66] },
-        '83':  { yellow: [50,59],   orange: [62,71],   red: [72,93] },
+        '53':  { yellow: [20,31],   orange: [40,51],   red: [60,60] },
+        '83':  { yellow: [50,59],   orange: [60,71],   red: [72,93] },
         '102': { yellow: [100,100] },
-        '61':  { yellow: [15,18],   orange: [19,25],   red: [26,35] },
-        '59':  { yellow: [5,8],     red: [17,18] },
-        '84':  { yellow: [50,59],   orange: [62,70],   red: [71,87] },
-        '101': { yellow: [25,28],   orange: [30,37],   red: [38,49] },
+        '61':  { yellow: [15,19],   orange: [19,25],   red: [26,35] },
+        '59':  { yellow: [5,8],     orange: [10,10],   red: [17,18] },
+        '84':  { yellow: [50,60],   orange: [60,71],   red: [77,81] },
+        '101': { yellow: [25,29],   orange: [30,35],   red: [38,49] },
         '21':  { yellow: [20,25],   orange: [26,33],   red: [36,49] },
-        '68':  { yellow: [15,21],   orange: [22,32],   red: [34,49] },
-        '14':  { yellow: [20,28],   orange: [29,38],   red: [44,59] },
-        '66':  { yellow: [20,27],   orange: [29,39],   red: [41,57] },
+        '68':  { yellow: [15,21],   orange: [22,32],   red: [33,43] },
+        '14':  { yellow: [20,28],   orange: [28,38],   red: [44,51] },
+        '66':  { yellow: [20,27],   orange: [28,39],   red: [45,51] },
         '88':  { yellow: [50,88],   orange: [91,149],  red: [154,219] },
-        '65':  { yellow: [4,5],     orange: [6,10],    red: [11,18] },
-        '41':  { yellow: [10,12],   orange: [13,17],   red: [18,24] },
-        '43':  { yellow: [50,69],   orange: [76,90],   red: [132,132] },
-        '44':  { yellow: [20,28],   orange: [29,41],   red: [43,64] },
+        '65':  { yellow: [4,6],     orange: [6,9],     red: [12,15] },
+        '41':  { yellow: [10,13],   orange: [13,17],   red: [18,24] },
+        '43':  { yellow: [50,69],   orange: [76,107],  red: [132,132] },
+        '44':  { yellow: [20,28],   orange: [29,42],   red: [43,61] },
         '73':  { yellow: [1,1],     orange: [2,2],     red: [4,4] },
-        '71':  { yellow: [20,27],   orange: [28,37],   red: [40,52] },
-        '20':  { yellow: [30,43],   orange: [44,54],   red: [85,96] },
-        '58':  { yellow: [10,15],   orange: [16,23],   red: [25,40] },
-        '60':  { yellow: [25,31],   orange: [33,40],   red: [41,49] },
-        '78':  { yellow: [3,4],     orange: [5,7],     red: [8,11] },
+        '71':  { yellow: [20,26],   orange: [28,37],   red: [40,59] },
+        '20':  { yellow: [31,43],   orange: [44,54],   red: [69,85] },
+        '58':  { yellow: [10,15],   orange: [16,24],   red: [30,40] },
+        '60':  { yellow: [25,31],   orange: [30,38],   red: [49,49] },
+        '78':  { yellow: [3,4],     orange: [5,6],     red: [8,11] },
         '48':  { yellow: [50,71],   orange: [76,105],  red: [119,170] },
-        '81':  { yellow: [15,19],   orange: [20,27],   red: [28,45] },
+        '81':  { yellow: [15,20],   orange: [20,27],   red: [28,38] },
         '46':  { yellow: [20,28],   orange: [29,40],   red: [44,63] },
-        '76':  { yellow: [125,145], orange: [146,167], red: [177,221] },
-        '42':  { yellow: [20,28],   orange: [29,42],   red: [45,63] }
+        '76':  { yellow: [125,143], orange: [145,167], red: [186,221] },
+        '42':  { yellow: [20,28],   orange: [29,42],   red: [43,60] }
     };
 
     function getBonusColor(bonusId, level) {
